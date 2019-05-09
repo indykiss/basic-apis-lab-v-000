c3ee3db3d7db301e4b2769c75e4863d2ab012e9a
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-09 18:03:06 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,7 +8,7 @@ class RepositoriesController < ApplicationController
       @resp = Faraday.get 'https://developer.github.com/v3/search/#search-repositories' do |req|
         req.params['client-id'] = 'd39518d29d8d39a37017'
         req.params['client_secret'] = '02dbaf073d5a7a127693bdc3c833ca58833ff390'
-        req.params['q'] = 'search'
+        req.params['q'] = params[:query]
     end
       body = JSON.parse(@resp.body)
       if @resp.success?
