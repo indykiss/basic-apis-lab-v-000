189862c4e1b33858f73ba151e579fca7836fa855
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-09 18:00:23 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,8 +8,9 @@ class RepositoriesController < ApplicationController
       @resp = Faraday.get 'https://developer.github.com/v3/' do |req|
         req.params['client-id'] = 'd39518d29d8d39a37017'
         req.params['client_secret'] = '02dbaf073d5a7a127693bdc3c833ca58833ff390'
-        req.params['v'] = '20160201'
-        req.params['query'] = 'github'
+        req.params['q'] = 'search'
+        req.params['sort'] = 'github'
+        req.params['order'] = 'github'
     end
       body = JSON.parse(@resp.body)
       if @resp.success?
