fc65a0553cf90e6ee59f5a722660d0e0b195e077
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-09 18:05:32 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -9,13 +9,14 @@ class RepositoriesController < ApplicationController
         req.params['client_secret'] = '02dbaf073d5a7a127693bdc3c833ca58833ff390'
         req.params['q'] = params[:query]
     end
+    
       body = JSON.parse(@resp.body)
       if @resp.success?
         @repos = body["items"]
       else
         @error = body["meta"]["errorDetail"]
       end
-      
+
       rescue Faraday::ConnectionFailed
         @error = "There was a timeout. Please try again."
       render 'search'
