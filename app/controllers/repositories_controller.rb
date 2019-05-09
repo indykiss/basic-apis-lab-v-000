07174784a223534ed0d1b0bfdc0af76fc507565e
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-09 17:58:51 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,7 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-      @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      @resp = Faraday.get 'https://developer.github.com/v3/' do |req|
         req.params['client-id'] = client_id
         req.params['client_secret'] = client_secret
         req.params['v'] = '20160201'
